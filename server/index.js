a37adb6b9012d7b7b1184826402883319af33621
Thinh Kieu <thinh.kieu@sioux.asia>
2019-08-19 20:59:09 +0700

    Initialize server (debugger)

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 const config = require('config');
+const debug = require('debug')('servera:index');
 const morgan = require('morgan');
 const express = require('express');
 const app = express();
 
-console.log(config.get('name'));
+debug(config.get('name'));
 
 if (app.get('env') === 'development'){
     app.use(morgan('tiny'));
-    console.log('Morgan enabled...');
+    debug('Morgan enabled...');
 }
 
 app.get('/', (req, res) => {
@@ -15,5 +16,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000...');
+    debug('Listening on port 3000...');
 })
