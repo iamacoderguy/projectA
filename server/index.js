c5cfe004b410d3f9fb98d293539e6dc624daf083
Thinh Kieu <thinh.kieu@sioux.asia>
2019-08-19 20:49:43 +0700

    Initialize server (config and RESTful logger)

--- server/index.js
+++ server/index.js
@@ -1,5 +1,14 @@
+const config = require('config');
+const morgan = require('morgan');
 const express = require('express');
 const app = express();
+
+console.log(config.get('name'));
+
+if (app.get('env') === 'development'){
+    app.use(morgan('tiny'));
+    console.log('Morgan enabled...');
+}
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
