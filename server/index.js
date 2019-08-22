b95dcd24cc09ed15d1cae55abf46aa451df50749
Thinh Kieu <thinh.kieu@sioux.asia>
2019-08-22 19:51:19 +0700

    Initialize server (prepare for deployment)

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ console.info('To enable debug mode, set env DEBUG=servera:*');
 require('./startup/logging')(app);
 require('./startup/routes')(app);
 require('./startup/config')();
+require('./startup/prod')(app);
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
