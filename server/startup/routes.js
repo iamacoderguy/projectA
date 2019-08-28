509e5f072e870b1e03a8d9c6cf2dd12b4cc6471e
Thinh Kieu <thinh.kieu@sioux.asia>
2019-08-29 00:30:09 +0700

    simpleDashboard (finish dashboard with html and css)

--- server/startup/routes.js
+++ server/startup/routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const methodOverride = require('method-override');;
 const client = require('../routes/client');
 const files = require('../routes/files');
 const error = require('../middleware/error');
 
-module.exports = function(app) {
+module.exports = function (app) {
+    app.use(methodOverride('_method')); // override with POST having ?_method=DELETE/PUT/GET
+    app.use(express.urlencoded({ extended: false }));
     app.use(express.json());
     app.use('/', client);
     app.use('/api/files', files);
--- server/startup/templating.js
+++
@@ -1,5 +0,0 @@
-
-module.exports = function(app) {
-    app.set('view engine', 'pug');
-    app.set('views', './views');
-}
