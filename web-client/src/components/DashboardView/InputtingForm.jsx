f64f5125bef9ddc17853fe0c556e8962c9d4bf5f
luyendo <luyen.do@sioux.asia>
2019-09-04 16:57:42 +0700

    refactor accessibility

--- web-client/src/components/DashboardView/InputtingForm.jsx
+++ web-client/src/components/DashboardView/InputtingForm.jsx
@@ -2,13 +2,12 @@ import React, { Component } from 'react';
 import './InputtingFormStyle.css';
 
 class InputtingForm extends Component {
-    render() { 
-        const inputId = "newInput" + this.props.inputId;
+    render() {
         return (
             <form className="inputting-form" method={this.props.formMethod} action={this.props.apiUrl} target={this.props.formTarget} encType={this.props.enctype}>
                 <div className="form-group">
-                    <label><b>{this.props.labelText}</b></label>
-                    <input type={this.props.inputType} name={this.props.inputName} className="form-control" id={inputId} placeholder={this.props.inputPlaceHolder}/>
+                    <label htmlFor={this.props.inputId}><b>{this.props.labelText}</b></label>
+                    <input type={this.props.inputType} name={this.props.inputName} className="form-control" id={this.props.inputId} placeholder={this.props.inputPlaceHolder}/>
                 </div>
                 <button type="submit" className="btn">{this.props.btnText}</button>
             </form>
