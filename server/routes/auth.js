84226d9e4e5e6679a457f5515cfbe70635b35def
iamacoderguy <iamacoderguy@gmail.com>
2019-09-04 18:36:19 +0700

    apiDoc (generate documentation for apis)

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,6 +6,17 @@ const auth = require('../middleware/auth');
 const db_Clients = require('../models/db_Clients');
 const networkHelper = require('../helpers/networkHelper');
 
+/**
+ * @api {post} /api/auth/connect 1. connect to server
+ * @apiGroup B.auth
+ * @apiPermission none
+ *
+ * @apiDescription It will make a connection between the client and the server
+ * @apiSuccess (Success) {Number} status 200
+ * @apiSuccess (Success) {String} body The token
+ * @apiSuccessExample {string} Success-Response:
+ *          eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImQ2M2Q3MWVlOGU3Zjk2Y2ZlYTk4MTllOWI5ZGNiNWY0IiwiaXNBZG1pbiI6dHJ1ZSwiZXhwQ29kZSI6MTU2NzU5MjA2NDAwMCwiaWF0IjoxNTY3NTkyMDY0LCJleHAiOjE1Njc1OTI5NjR9.bqtY5HAA6CToZk6bVtIoGbZP880MdgtSZmksQ0QevZg
+ */
 router.post('/connect', (req, res) => {
     const ipAddr = networkHelper.getIpAddressFromReq(req);
     const client = db_Clients.getClient(ipAddr);
@@ -17,6 +28,14 @@ router.post('/connect', (req, res) => {
     res.status(200).send(jwt);
 })
 
+/**
+ * @api {post} /api/auth/disconnect 1. disconnect from server
+ * @apiGroup B.auth
+ * @apiPermission connected users
+ *
+ * @apiDescription It will remove the connection between the client and the server
+ * @apiSuccess (Success) {Number} status 200
+ */
 router.post('/disconnect', auth, (req, res) => {
     const ipAddr = networkHelper.getIpAddressFromReq(req);
     const client = db_Clients.getClient(ipAddr);
--- server/tests/integration/routes/client.test.js
+++ server/tests/integration/routes/client.test.js
@@ -1,4 +1,3 @@
-const each = require('jest-each').default;
 const request = require('supertest');
 let app;
 
@@ -9,44 +8,62 @@ describe('/', () => {
     afterEach(() => jest.resetModules());
 
     describe('GET /', () => {
-        each([['client is on localhost', true], ['client is NOT on localhost', false]]).it('should return 200 in case %s', async (_, casevalue) => {
-            // arrange
-            const networkHelper = require('../../../helpers/networkHelper');
-            networkHelper.isFromLocalhost = jest.fn().mockReturnValue(casevalue);
 
-            // act
-            const res = await request(app).get('/');
+        describe('when client is on localhost', () => {
+            it('should return 200', async () => {
+                // arrange
+                const networkHelper = require('../../../helpers/networkHelper');
+                networkHelper.isFromLocalhost = jest.fn().mockReturnValue(true);
+    
+                // act
+                const res = await request(app).get('/');
+    
+                // assert
+                expect(res.status).toBe(200);
+            })
 
-            // assert
-            expect(res.status).toBe(200);
+            it('should render adminDashboardView correctly', async () => {
+                // arrange
+                const serverIpAddr = '192.168.5.1';
+                const serverPort = 3000;
+                const networkHelper = require('../../../helpers/networkHelper');
+                networkHelper.getServerIpAddress = jest.fn().mockReturnValue(serverIpAddr);
+                networkHelper.getServerPort = jest.fn().mockReturnValue(serverPort);
+                networkHelper.isFromLocalhost = jest.fn().mockReturnValue(true);
+    
+                const sharedPath = 'path-to-files';
+                const sharedPathHelper = require('../../../helpers/sharedPathHelper');
+                sharedPathHelper.getSharedPath = jest.fn().mockReturnValue(sharedPath);
+    
+                const db_Clients = require('../../../models/db_Clients');
+                const Client = require('../../../models/client');
+                const client1 = new Client('111.222.333.444');
+                const client2 = new Client('111.222.333.555');
+                const client3 = new Client('111.222.333.666');
+                client1.expCode = 0; client2.expCode = NaN; client3.expCode = 2;
+                db_Clients.getClients = jest.fn().mockReturnValue([client1, client2, client3]);
+    
+                // act
+                const res = await request(app).get('/');
+    
+                // assert
+                expect(res.text).toMatchSnapshot();
+            })
         })
 
-        each([['client is on localhost', true], ['client is NOT on localhost', false]]).it('should render adminDashboardView correctly in case %s', async (_, casevalue) => {
-            // arrange
-            const serverIpAddr = '192.168.5.1';
-            const serverPort = 3000;
-            const networkHelper = require('../../../helpers/networkHelper');
-            networkHelper.getServerIpAddress = jest.fn().mockReturnValue(serverIpAddr);
-            networkHelper.getServerPort = jest.fn().mockReturnValue(serverPort);
-            networkHelper.isFromLocalhost = jest.fn().mockReturnValue(casevalue);
-
-            const sharedPath = 'path-to-files';
-            const sharedPathHelper = require('../../../helpers/sharedPathHelper');
-            sharedPathHelper.getSharedPath = jest.fn().mockReturnValue(sharedPath);
-
-            const db_Clients = require('../../../models/db_Clients');
-            const Client = require('../../../models/client');
-            const client1 = new Client('111.222.333.444');
-            const client2 = new Client('111.222.333.555');
-            const client3 = new Client('111.222.333.666');
-            client1.expCode = 0; client2.expCode = NaN; client3.expCode = 2;
-            db_Clients.getClients = jest.fn().mockReturnValue([client1, client2, client3]);
-
-            // act
-            const res = await request(app).get('/');
-
-            // assert
-            expect(res.text).toMatchSnapshot();
+        describe('when client is NOT on localhost', () => {
+            it('should redirect to apidoc/index.html', async () => {
+                // arrange
+                const networkHelper = require('../../../helpers/networkHelper');
+                networkHelper.isFromLocalhost = jest.fn().mockReturnValue(false);
+    
+                // act
+                const res = await request(app).get('/');
+    
+                // assert
+                expect(res.status).toBe(302);
+                expect(res.header['location']).toBe('apidoc/index.html');
+            })
         })
     })
 })
