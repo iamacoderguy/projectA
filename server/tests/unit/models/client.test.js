e621c66a238a2df3451a9128a37b612bc289a281
iamacoderguy <iamacoderguy@gmail.com>
2019-09-05 15:59:28 +0700

    connectionPIN (more unit tests for models/client)

--- server/tests/unit/models/client.test.js
+++ server/tests/unit/models/client.test.js
@@ -30,3 +30,89 @@ describe('generateAuthToken', () => {
         });
     })
 })
+
+describe('createNewSession', () => {
+    it('should change expCode to current moment', () => {
+        // arrange
+        const now = Date.now();
+        const expectedIpAddr = "123.5.1.5";
+        const client = new Client(expectedIpAddr);
+
+        // act
+        client.createNewSession();
+
+        // assert
+        expect(typeof client.expCode).toBe("number");
+        expect(client.expCode).toBeGreaterThanOrEqual(now);
+    })
+})
+
+describe('cleanAllSessions', () => {
+    it('should change expCode to NaN', () => {
+        // arrange
+        const expectedIpAddr = "123.5.1.5";
+        const client = new Client(expectedIpAddr);
+        client.createNewSession();
+
+        // act
+        client.cleanAllSessions();
+
+        // assert
+        expect(client.expCode).toBeNaN();
+    })
+})
+
+describe('getIpAddr', () => {
+    it('should return the decrypted client ip address', () => {
+        // arrange
+        const expectedIpAddr = "123.5.1.5";
+        const client = new Client(expectedIpAddr);
+
+        // act
+        const actualIp = client.getIpAddr();
+
+        // assert
+        expect(actualIp).toBe(expectedIpAddr);
+    })
+})
+
+describe('getStatus', () => {
+    it('should return disconnected if NO session is set up, yet', () => {
+        // arrange
+        const expectedIpAddr = "123.5.1.5";
+        const client = new Client(expectedIpAddr);
+
+        // act
+        const status = client.getStatus();
+
+        // assert
+        expect(status).toBe('disconnected');
+    })
+
+    it('should return disconnected if all active sessions are reset', () => {
+        // arrange
+        const expectedIpAddr = "123.5.1.5";
+        const client = new Client(expectedIpAddr);
+        client.createNewSession();
+        client.cleanAllSessions();
+
+        // act
+        const status = client.getStatus();
+
+        // assert
+        expect(status).toBe('disconnected');
+    })
+
+    it('should return connecting if there is any active sessions', () => {
+        // arrange
+        const expectedIpAddr = "123.5.1.5";
+        const client = new Client(expectedIpAddr);
+        client.createNewSession();
+
+        // act
+        const status = client.getStatus();
+
+        // assert
+        expect(status).toBe('connecting');
+    })
+})
