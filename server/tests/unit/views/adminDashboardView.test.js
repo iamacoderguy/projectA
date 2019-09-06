8737c6125d6a4afd9ba7e1ed0153bd3541999553
iamacoderguy <53971138+iamacoderguy@users.noreply.github.com>
2019-09-06 20:03:00 +0700

    Make view snapshot tests unit (#53)

--- server/tests/unit/views/adminDashboardView.test.js
+++ server/tests/unit/views/adminDashboardView.test.js
@@ -5,10 +5,17 @@ const renderPug = (view, data) => pug.renderFile(view, data);
 const Client = require('../../../models/client');
 
 describe('adminDashboardView', () => {
-    const client1 = new Client('111.222.333.444');
-    const client2 = new Client('111.222.333.555');
-    const client3 = new Client('111.222.333.666');
-    client1.expCode = 0; client2.expCode = NaN; client3.expCode = 2;
+    const client1 = {};
+    client1.getIpAddr = () => '111.222.333.444';
+    client1.getStatus = () => 'status1';
+
+    const client2 = {};
+    client2.getIpAddr = () => '111.222.333.555';
+    client2.getStatus = () => 'status2';
+
+    const client3 = {};
+    client3.getIpAddr = () => '111.222.333.666';
+    client3.getStatus = () => 'status3';
 
     each([
         [[client1, client2, client3]],
@@ -16,8 +23,8 @@ describe('adminDashboardView', () => {
     ]).it('should render adminDashboardView correctly in case %o', (clients) => {
         expect(renderPug('views/adminDashboardView.pug', {
             path: 'path-to-files',
-            dashboardName: 'dashboard-name',
-            ipAddress: 'ip-address',
+            dashboardName: 'Dashboard',
+            ipAddress: '192.168.1.1',
             port: 5000,
             clients: clients
           })).toMatchSnapshot();
