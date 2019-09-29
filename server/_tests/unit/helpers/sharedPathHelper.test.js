c9663dcbadaead54c1a7fe0a4d88a06c1b0a236b
iamacoderguy <iamacoderguy@gmail.com>
2019-09-29 19:52:30 +0700

    Improve sharedPathHelper.test.js

--- server/_tests/unit/helpers/sharedPathHelper.test.js
+++ server/_tests/unit/helpers/sharedPathHelper.test.js
@@ -1,30 +1,35 @@
-const { setSharedPath, getSharedPath } = require('../../../helpers/sharedPathHelper');
 const each = require('jest-each').default;
+let setSharedPath, getSharedPath;
 
-describe('setSharedPath', () => {
-    each([[''], ['path/to/file'], ['../path/to/file']]).it('should set \'%s\' to the sharedPath', (path) => {
-        // arrange
-        const newSetSharedPath = require('../../../helpers/sharedPathHelper').setSharedPath;
+describe('sharedPathHelper', () => {
+    beforeEach(() => {
+        const sharedPathHelper = require('../../../helpers/sharedPathHelper');
+        setSharedPath = sharedPathHelper.setSharedPath;
+        getSharedPath = sharedPathHelper.getSharedPath;
+    });
+    afterEach(() => jest.resetModules());
 
-        // act
-        newSetSharedPath(path);
-
-        // assert
-        const theSharedPath = getSharedPath();
-        expect(theSharedPath).toBe(path);
+    describe('setSharedPath', () => {
+        each([[''], ['path/to/file'], ['../path/to/file']]).it('should set \'%s\' to the sharedPath', (path) => {
+            // act
+            setSharedPath(path);
+    
+            // assert
+            const theSharedPath = getSharedPath();
+            expect(theSharedPath).toBe(path);
+        })
     })
-})
 
-describe('getSharedPath', () => {
-    each([[''], ['path/to/file'], ['../path/to/file']]).it('should return the sharedPath in case path=\'%s\'', (path) => {
-        // arrange
-        setSharedPath(path);
-        const newGetSharedPath = require('../../../helpers/sharedPathHelper').getSharedPath;
-
-        // act
-        const theSharedPath = newGetSharedPath();
-
-        // assert
-        expect(theSharedPath).toBe(path);
+    describe('getSharedPath', () => {
+        each([[''], ['path/to/file'], ['../path/to/file']]).it('should return the sharedPath in case path=\'%s\'', (path) => {
+            // arrange
+            setSharedPath(path);
+    
+            // act
+            const theSharedPath = getSharedPath();
+    
+            // assert
+            expect(theSharedPath).toBe(path);
+        })
     })
 })
